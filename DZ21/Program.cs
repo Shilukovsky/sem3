a68121beef2167234a9b52408313c7f3c6f1caa2
Nikolay <cccp_ussr@bk.ru>
2022-07-16 09:44:39 +0300

    added DZ21

---
+++ DZ21/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("введите координаты точки А");
+Console.Write("XA: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("YA: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("ZA: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координаты точки B");
+Console.Write("XB: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("YB: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("ZB: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double dlina(int xac, int yac, int zac, int xbc, int ybc, int zbc)
+{
+    double ab = Math.Sqrt((xbc - xac) * (xbc - xac) + (ybc - yac) * (ybc - yac) + (zbc - zac) * (zbc - zac)); 
+    return(ab);
+}
+
+double result = dlina(xa, ya, za, xb, yb, zb);
+Console.WriteLine(Math.Round (result,2));
